cdc8d46374a28bc334ef72fc4f680df45f82ed5b
Alexis Prevost-Maynen <alexis@callbruno.com>
2023-09-29 11:49:54 +0200

    Update - Components - Layout - Try shake phone

--- components/layout.jsx
+++ components/layout.jsx
@@ -4,24 +4,44 @@ import DevTools from './DevTools'
 import { useEffect, useState } from 'react'
 
 export default function Layout({ children, header, footer }) {
-  const [isEscape, setIsEscape] = useState(false)
+  const [isDevTools, setIsDevTools] = useState(false)
+
+  const handleMotion = (event) => {
+    const { accelerationIncludingGravity } = event
+
+    const totalAcceleration = Math.sqrt(
+      accelerationIncludingGravity.x ** 2 +
+        accelerationIncludingGravity.y ** 2 +
+        accelerationIncludingGravity.z ** 2
+    )
+
+    const shakeThreshold = 15
+
+    if (totalAcceleration > shakeThreshold) {
+      setIsDevTools(true)
+    } else {
+      setIsDevTools(false)
+    }
+  }
 
   const handleKeyPress = (event) => {
     if (event.key === 'Escape') {
-      setIsEscape(!isEscape)
+      setIsDevTools(!isDevTools)
     }
   }
 
   useEffect(() => {
     document.addEventListener('keydown', handleKeyPress)
+    window.addEventListener('devicemotion', handleMotion)
     return () => {
       document.removeEventListener('keydown', handleKeyPress)
+      window.removeEventListener('devicemotion', handleMotion)
     }
   })
 
   return (
     <>
-      {isEscape && <DevTools />}
+      {isDevTools && <DevTools />}
       {/* <SEO title={title} description={description} image={image} /> */}
       <Header header={header} />
       <main>{children}</main>

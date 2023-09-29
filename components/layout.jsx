94bba6ed3a40395f4960348d8cbe5090000a7a6f
Alexis Prevost-Maynen <alexis@callbruno.com>
2023-09-29 11:12:10 +0200

    Update - Components - Layout - Add event for activate devtools

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,11 +1,27 @@
 import Footer from './Footer'
 import Header from './Header'
 import DevTools from './DevTools'
+import { useEffect, useState } from 'react'
 
 export default function Layout({ children, header, footer }) {
+  const [isEscape, setIsEscape] = useState(false)
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Escape') {
+      setIsEscape(!isEscape)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress)
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress)
+    }
+  })
+
   return (
     <>
-      <DevTools />
+      {isEscape && <DevTools />}
       {/* <SEO title={title} description={description} image={image} /> */}
       <Header header={header} />
       <main>{children}</main>

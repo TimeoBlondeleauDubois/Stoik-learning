03c235de52acea341164464e93cf58664109cfa8
Alexis Prevost-Maynen <alexis@callbruno.com>
2023-09-29 09:53:08 +0200

    Update - Components - Layout - Import DevTools & Fix import SEO

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,11 +1,12 @@
-import { SliceZone } from '@prismicio/react'
-// import { components } from '../slices'
 import Footer from './Footer'
 import Header from './Header'
+import DevTools from './DevTools'
+
 export default function Layout({ children, header, footer }) {
   return (
     <>
-      {/* <SliceZone slices={seo} components={components} /> */}
+      <DevTools />
+      {/* <SEO title={title} description={description} image={image} /> */}
       <Header header={header} />
       <main>{children}</main>
       <Footer variation={'default'} footer={footer} />

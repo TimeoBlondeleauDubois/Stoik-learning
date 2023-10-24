78031b01069de2393d89520604c37135c015f601
Maxime Cottin <max.cttn@gmail.com>
2023-10-24 16:39:22 +0200

    Update - Component - footerv1 - Clean code and fix margn

--- components/layout.jsx
+++ components/layout.jsx
@@ -44,9 +44,9 @@ export default function Layout({ children, header, footer }) {
       <Header header={header} />
       <main>{children}</main>
       {/* <Footer variation={'default'} footer={footer} /> */}
-      {/* <Footer variation={'variant1'} footer={footer} /> */}
+      <Footer variation={'variant1'} footer={footer} />
       {/* <Footer variation={'variant2'} footer={footer} /> */}
-      <Footer variation={'variant3'} footer={footer} />
+      {/* <Footer variation={'variant3'} footer={footer} /> */}
       {/* <Footer variation={'variant4'} footer={footer} /> */}
     </>
   )

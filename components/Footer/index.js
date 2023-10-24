eb679579deab13ebe9651e0a4c4b0a6329140ae7
Maxime Cottin <max.cttn@gmail.com>
2023-10-24 12:24:38 +0200

    Add - Component - footerv4

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,9 +1,12 @@
 import FooterDefault from './default'
 import FooterVariant1 from './variant1'
 import FooterVariant2 from './variant2'
+import FooterVariant4 from './variant4'
 
 const Footer = ({ variation }) => {
   switch (variation) {
+    case 'variant4':
+      return <FooterVariant4 />
     case 'variant2':
       return <FooterVariant2 />
     case 'variant1':
--- components/layout.jsx
+++ components/layout.jsx
@@ -43,9 +43,10 @@ export default function Layout({ children, header, footer }) {
       {/* <SEO title={title} description={description} image={image} /> */}
       <Header header={header} />
       <main>{children}</main>
-      <Footer variation={'default'} footer={footer} />
-      <Footer variation={'variant1'} footer={footer} />
-      <Footer variation={'variant2'} footer={footer} />
+      {/* <Footer variation={'default'} footer={footer} /> */}
+      {/* <Footer variation={'variant1'} footer={footer} /> */}
+      {/* <Footer variation={'variant2'} footer={footer} /> */}
+      <Footer variation={'variant4'} footer={footer} />
     </>
   )
 }

cc877db0ec48670feb957516cc8d0045928437ab
Maxime Cottin <max.cttn@gmail.com>
2023-10-24 15:14:00 +0200

    Add - Component - footerv3

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,12 +1,15 @@
 import FooterDefault from './default'
 import FooterVariant1 from './variant1'
 import FooterVariant2 from './variant2'
+import FooterVariant3 from './variant3'
 import FooterVariant4 from './variant4'
 
 const Footer = ({ variation }) => {
   switch (variation) {
     case 'variant4':
       return <FooterVariant4 />
+    case 'variant3':
+      return <FooterVariant3 />
     case 'variant2':
       return <FooterVariant2 />
     case 'variant1':

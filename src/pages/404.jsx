a59721b9dec6a85202490ce3069ff7bdf9591b6f
Alexis Prevost-Maynen <alexis@callbruno.com>
2024-01-31 09:31:43 +0100

    Update - Pages - 404 - Fix bug

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,12 +1,9 @@
 import { ApiService } from '/services/api.service'
-import { CustomService } from '/services/custom.service'
 import { PageService } from '/services/page.service'
 import { getLangFromLocale } from '/utils/get-lang-from-locale'
 
 import Layout from '/components/layout.jsx'
 import SEO from '/components/SEO'
-
-import styles from '@/styles/pages/404.module.scss'
 
 const p404 = ({ header, footer, page }) => {
   const data = page.data
@@ -27,19 +24,11 @@ export default p404
 export async function getStaticProps({ locale, previewData }) {
   ApiService.setPreviewData({ previewData })
   const lang = getLangFromLocale(locale)
-  const customService = new CustomService(lang)
   const pageService = new PageService(lang)
-  const [header, footer, page] = await Promise.all([
-    customService.getHeader(),
-    customService.getFooter(),
-    pageService.get404(),
-  ])
+  const [page] = await Promise.all([pageService.get404()])
   return {
     props: {
-      header,
-      footer,
       page,
     },
-    revalidate: 180,
   }
 }

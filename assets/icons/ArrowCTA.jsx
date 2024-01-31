70bb5659cd35828995f2df861ad6567a7ce02040
Alexis Prevost-Maynen <alexis@callbruno.com>
2024-01-31 09:03:54 +0100

    Update - Assets - Icons - ArrowCTA - Rework

--- assets/icons/ArrowCTA.jsx
+++ assets/icons/ArrowCTA.jsx
@@ -1,8 +1,15 @@
-export const ArrowCTA = () => {
+export default function ArrowCTA({ className, content }) {
   return (
-    <svg viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg">
-      <path d="M4.08203 0.5L7.99804 4.5L4.08203 8.5" />
-      <path d="M8 4.5H1" />
+    <svg
+      viewBox="0 0 9 10"
+      className={className}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id={content}>
+        <path d="M4.00391 1L7.91992 5L4.00391 9" />
+        <path d="M7.92188 5H0.921875" />
+      </g>
     </svg>
-  );
-};
+  )
+}

39bb22607d6c54ed442ea1539326b9032ebc6de2
Alexis Prevost-Maynen <alexis@callbruno.com>
2024-01-31 11:45:50 +0100

    Update - Services - Custom - JS -> TS & rework function

---
+++ services/custom.service.ts
@@ -0,0 +1,19 @@
+import { AllDocumentTypes } from '../prismicio-types'
+import { ApiService } from './api.service'
+
+export class CustomService {
+  apiService: ApiService
+
+  constructor(private readonly lang: string) {
+    this.apiService = ApiService.getInstance()
+    this.lang = lang
+  }
+
+  async getFooter(): Promise<AllDocumentTypes> {
+    return await this.apiService.getSinglePage('footer', this.lang)
+  }
+
+  async getHeader(): Promise<AllDocumentTypes> {
+    return await this.apiService.getSinglePage('header', this.lang)
+  }
+}

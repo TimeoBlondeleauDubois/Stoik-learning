de18795e887a1cc51a60a2919d709ca25f1bb2cd
TheoDosSantos <theo@callbruno.com>
2024-04-04 16:42:49 +0200

    Update - Components - SEO - Adding SEO Optimization

--- components/SEO/index.jsx
+++ components/SEO/index.jsx
@@ -1,9 +1,19 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 const SEO = ({ seo }) => {
   if (!seo) return
 
-  const { title, description, image } = seo
+  const router = useRouter()
+  const { locale, asPath } = router
+  const { title, description, image, altPage, currentPath, page } = seo
+
+  const rootPath = 'https://www.callbruno.com' // Replace with client's domain
+
+  const canonical = `${rootPath}${currentPath}`.split(/[?#]/)[0]
+
+  const isOriginalFolder = asPath.indexOf(`/${page.type}/${page.uid}`) > -1
+
   return (
     <>
       <Head>
@@ -14,12 +24,15 @@ const SEO = ({ seo }) => {
         {/* <!-- Open Graph / Facebook --> */}
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content={image} />
+        {image && <meta property="og:image" content={image.url} />}
         {/* <!-- Twitter --> */}
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:title" content={title} />
         <meta property="twitter:description" content={description} />
-        <meta property="twitter:image" content={image} />
+        {image && <meta property="twitter:image" content={image.url} />}
+        {/* <!-- SEO Optimizations --> */}
+        {isOriginalFolder && <meta name="robots" content="noindex" />}
+        <link rel="canonical" href={canonical} />
         {/* <!-- Favicon --> */}
       </Head>
     </>

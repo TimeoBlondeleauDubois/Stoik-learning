171e4a5090ede84f2880032e033cf632e3089498
Blondeleau <timeo.blondeleaudubois@viacesi.fr>
2024-05-02 15:57:08 +0200

    Mise à jour du composant Header pour supprimer le composant Eaqual et utiliser un SVG inline à la place

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText } from "@prismicio/react";
 import styles from "./style.module.scss";
-import Eaqual from "../eaqual";
 
 const Header = ({ header }) => {
   let i = 0;
@@ -110,7 +109,10 @@ const Header = ({ header }) => {
             </div>
           </div>
           <div className={styles.ButtonLogoMobile} onClick={toggleContainerMobile}>
-            <Eaqual/> 
+            <svg id="all" width="16" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path id="path1" d="M1 6.8125H15M1" stroke="#3C1441" stroke-width="1.5" stroke-linecap="square" stroke-linejoin="round"/>
+              <path id="path2" d="M1 0.8125H15" stroke="#3C1441" stroke-width="1.5" stroke-linecap="square" stroke-linejoin="round"/>
+            </svg>
           </div>
           <div className={styles.ContainerMobile}>
             <div className={styles.Mobile}>

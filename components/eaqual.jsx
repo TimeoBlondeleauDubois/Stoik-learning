1cc8a50b2d4680099cc8d401ac95bdda847852c2
Blondeleau <timeo.blondeleaudubois@viacesi.fr>
2024-05-02 14:45:38 +0200

    Mise à jour du composant Eaqual pour corriger les erreurs de syntaxe et les attributs d'éléments SVG

--- components/eaqual.jsx
+++ components/eaqual.jsx
@@ -1,10 +1,10 @@
 import "./styles.module.scss";
 
-export default function eaqual() {
-    return (
-      <svg width="16" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M1 6.8125H15M1" stroke="#3C1441" stroke-width="1.5" stroke-linecap="square" stroke-linejoin="round"/>
-      <path d="M1 0.8125H15" stroke="#3C1441" stroke-width="1.5" stroke-linecap="square" stroke-linejoin="round"/>
-      </svg>
-    );
-  }
+export default function Equal() {
+  return (
+    <svg width="16" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M1 6.8125H15" stroke="#3C1441" strokeWidth="1.5" strokeLinecap="square" strokeLinejoin="round"/>
+      <path d="M1 0.8125H15" stroke="#3C1441" strokeWidth="1.5" strokeLinecap="square" strokeLinejoin="round"/>
+    </svg>
+  );
+}

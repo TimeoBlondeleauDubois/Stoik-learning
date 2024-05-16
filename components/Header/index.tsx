087727bd830884ffe1fba91a168b83726ddeda08
Blondeleau <timeo.blondeleaudubois@viacesi.fr>
2024-05-16 17:06:28 +0200

    test

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,6 +3,9 @@ import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText } from "@prismicio/react";
 import styles from "./style.module.scss";
 
+
+/*test*/
+
 const Header = ({ currentPage, header }) => {
   let i = 0;
   const toggleContainerMobile = () => {

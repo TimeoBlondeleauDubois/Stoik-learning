4be6a446e4cd8c026f26242ba79937cf571d235d
Blondeleau <timeo.blondeleaudubois@viacesi.fr>
2024-05-16 11:12:02 +0200

    feat: Lazy load images and improve styling in FifthSection

--- src/sections/slices/FifthSection/index.js
+++ src/sections/slices/FifthSection/index.js
@@ -40,11 +40,6 @@ const FifthSection = ({ slice }) => {
                   <PrismicNextImage key={index} className={styles.logo} field={item.logo} />
                 </div>
               ))}
-              {slice.items.map((item, index) => (
-                <div key={index} className={styles.logocontainer}>
-                  <PrismicNextImage key={index} className={styles.logo} field={item.logo} />
-                </div>
-              ))}
             </div>
           </div>
         </div>

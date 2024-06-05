712bf16cce667015d39dcf9cc33114e26c03359a
Blondeleau <timeo.blondeleaudubois@viacesi.fr>
2024-06-05 18:17:14 +0200

    chore: Update blog page styles for better layout and readability

--- src/pages/blog/blog.jsx
+++ src/pages/blog/blog.jsx
@@ -9,7 +9,7 @@ import { components as componentsHeros } from '@/sections/heros';
 import { components as componentsSlices } from '@/sections/slices';
 import { components as componentsBruno } from '@/sections/bruno';
 import Link from 'next/link';
-/* import styles from './styles.module.scss' */
+import styles from './styles.module.scss';
 
 export default function Home({ page, header, footer, categories, articles }) { 
   const [selectedCategory, setSelectedCategory] = useState(null);
@@ -23,8 +23,13 @@ export default function Home({ page, header, footer, categories, articles }) {
     ? articles.filter(article => article.data.categorie.id === selectedCategory)
     : articles;
 
+  const getCategoryName = (categoryId) => {
+    const category = categories.find(categorie => categorie.id === categoryId);
+    return category ? category.data.name : 'Catégorie inconnue';
+  };
+
   return (
-    <Layout header={header} footer={footer} currentPage="blog">
+<Layout header={header} footer={footer} currentPage="blog">
       <SliceZone
         slices={data.slices}
         components={{
@@ -33,37 +38,48 @@ export default function Home({ page, header, footer, categories, articles }) {
           ...componentsBruno,
         }}
       />
-      <div>
-        <ul>
-          <button onClick={() => setSelectedCategory(null)}>
-            Tous
-          </button>
-          {categories.map(categorie => (
-            <li key={categorie.id}>
-              <button onClick={() => setSelectedCategory(categorie.id)}>
-                {categorie.data.name}
+      <div className={styles.blog_main}>
+        <div>
+          <ul>
+            <div className={styles.button_categories_container}>
+              <button
+                className={`${styles.button_tous} ${selectedCategory === null ? styles.selected : ''}`}
+                onClick={() => setSelectedCategory(null)}
+              >
+                Tous
               </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <ul>
-          {filteredArticles.map(article => (
-            <li key={article.id}>
-              <Link href={`/article/${article.uid}`} legacyBehavior>
-                <a>
-                  <p>id de l'article: {article.uid}</p>
-                  <p>id de la catégorie présent dans l'article: {article.data.categorie.id}</p>
-                  <p>nom de la catégorie présent dans l'article: {article.data.categorie.slug}</p>
-                  <p>titre de l'article: {article.data.titlearticle[0].text}</p>
-                  <p>description de l'article: {article.data.descriptionarticle[0].text}</p>
-                  <br />
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
+              {categories.map(categorie => (
+                <li key={categorie.id}>
+                  <button
+                    className={`${styles.button_categories} ${selectedCategory === categorie.id ? styles.selected : ''}`}
+                    onClick={() => setSelectedCategory(categorie.id)}
+                  >
+                    {categorie.data.name}
+                  </button>
+                </li>
+              ))}
+            </div>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            {filteredArticles.map(article => (
+              <li key={article.id} className={styles.articles_container}>
+                <Link href={`/article/${article.uid}`} legacyBehavior>
+                  <a>
+                    <div className={styles.wrapper}>
+                      <p className={styles.articles_title}>{article.data.titlearticle[0].text}</p>
+                      <p className={styles.articles_description}>{article.data.descriptionarticle[0].text}</p>
+                      <div className={styles.articles_name_categorie_container}>
+                        <p className={styles.articles_name_categorie}>{getCategoryName(article.data.categorie.id)}</p>
+                      </div>
+                    </div>
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </Layout>
   );
